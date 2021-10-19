9a577c671e49418a2508e37d29b9a485273ce9e3
Stefan Hagen <stefan@hagen.link>
2021-10-19 07:59:31 +0200

    Update __main__.py

--- kysy/__main__.py
+++ kysy/__main__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import sys
 
+from kysy.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app(prog_name='foran'))  # pragma: no cover

cafbd6ed3dfd597d4678f17bf316b59e977898f4
Stefan Hagen <stefan@hagen.link>
2021-10-19 07:58:59 +0200

    Update test_kysy.py

--- tests/test_kysy.py
+++ tests/test_kysy.py
@@ -1,1 +1,14 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import pathlib
 
+import pytest
+
+import kysy.kysy as ky
+
+
+def test_main_empty():
+    message = 'received wrong number of arguments'
+    with pytest.raises(UserWarning) as ex:
+        ky.main([]) == 2
+        assert message in str(ex.value)

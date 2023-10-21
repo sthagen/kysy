8502b43376d1f3486010c91dc5a27a739fd402f8
Stefan Hagen <stefan@hagen.link>
2023-10-21 23:08:30 +0200

    Bumped impl version for release 2023.10.21

--- kysy/__init__.py
+++ kysy/__init__.py
@@ -1,7 +1,7 @@
 """Ask or know (kysy tai tiedä). Package interface."""
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.21+parent.cd0a7c25'
+# [[[end]]] (checksum: 14fb4875ac40bc1a60deee9b5bbfde58)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )

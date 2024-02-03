40d2724947d759961bd7661a89b0b77d4a7fb374
Stefan Hagen <stefan@hagen.link>
2024-02-03 16:12:57 +0100

    Happy linter, warmer winter and fixed verbosity mgmnt
    Signed-off-by: Stefan Hagen <stefan@hagen.link>

--- kysy/__init__.py
+++ kysy/__init__.py
@@ -1,4 +1,5 @@
 """Ask or know (kysy tai tiedä). Package interface."""
+
 # [[[fill git_describe()]]]
 __version__ = '2023.10.21+parent.cd0a7c25'
 # [[[end]]] (checksum: 14fb4875ac40bc1a60deee9b5bbfde58)
